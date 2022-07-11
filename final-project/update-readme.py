2b21b89e0b33c67e2375708e3304b50048ad17e9
soyoung <psy99326@gamil.com>
2022-07-11 15:55:50 +0900

    design index.md

--- final-project/update-readme.py
+++ final-project/update-readme.py
@@ -22,7 +22,7 @@ if(os.path.isfile(os.path.join(BASE_DIR, 'news_latest_one.json'))):
     prev_url=prev_item['url']
     prev_sec=prev_item['sec']
     readme.write("### section : " + prev_sec+"\n\n")
-    readme.write("```markdown\n\n")
+    readme.write("|")
     # 업데이트 되었는지 확인
     if 'name' in new_item:
       new_title=new_item['name']
@@ -36,6 +36,6 @@ if(os.path.isfile(os.path.join(BASE_DIR, 'news_latest_one.json'))):
       readme.write("새롭게 업데이트 된 뉴스가 없습니다.\n\n")
       
     readme.write("이전 뉴스 : [" + prev_title + "]("+ prev_url + ")\n\n")
-    readme.write("```\n\n")
+    readme.write("|")
     readme.write("\n\n\n\n")
     readme.close()
